48da8cb89c18d89242a57e2978e3fbaf33906572
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-09-17 00:03:40 +0300

    Add toolbar title

--- app/src/main/java/com/nazlican/deezer/ui/favoritemusic/FavoriteMusicFragment.kt
+++ app/src/main/java/com/nazlican/deezer/ui/favoritemusic/FavoriteMusicFragment.kt
@@ -3,6 +3,7 @@ package com.nazlican.deezer.ui.favoritemusic
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.nazlican.deezer.R
 import com.nazlican.deezer.common.viewBinding
 import com.nazlican.deezer.databinding.FragmentFavoriteMusicBinding
@@ -13,6 +14,9 @@ class FavoriteMusicFragment : Fragment(R.layout.fragment_favorite_music) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.musicFavoriteToolbar.title = "Favorite Music"
+        (activity as AppCompatActivity).setSupportActionBar(binding.musicFavoriteToolbar)
     }
 
 }
--- app/src/main/java/com/nazlican/deezer/ui/musiccategory/MusicCategoryFragment.kt
+++ app/src/main/java/com/nazlican/deezer/ui/musiccategory/MusicCategoryFragment.kt
@@ -3,6 +3,7 @@ package com.nazlican.deezer.ui.musiccategory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.nazlican.deezer.R
 import com.nazlican.deezer.common.viewBinding
 import com.nazlican.deezer.databinding.FragmentMusicCategoryBinding
@@ -10,9 +11,11 @@ import com.nazlican.deezer.databinding.FragmentMusicCategoryBinding
 class MusicCategoryFragment : Fragment(R.layout.fragment_music_category) {
 
     private val binding by viewBinding(FragmentMusicCategoryBinding::bind)
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.musicCategoryToolbar.title = "Music Category"
+        (activity as AppCompatActivity).setSupportActionBar(binding.musicCategoryToolbar)
     }
 
 }
