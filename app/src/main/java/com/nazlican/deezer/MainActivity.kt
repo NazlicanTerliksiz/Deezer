1fe68f830e62d7d7f2792992b7ee8cba0a7a97a0
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-09-17 00:03:15 +0300

    Add bottom navigation bar

--- app/src/main/java/com/nazlican/deezer/MainActivity.kt
+++ app/src/main/java/com/nazlican/deezer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nazlican.deezer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.nazlican.deezer.common.viewBinding
 import com.nazlican.deezer.databinding.ActivityMainBinding
 
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(
+            binding.bottomNav,
+            navHostFragment.navController
+        )
     }
 }
